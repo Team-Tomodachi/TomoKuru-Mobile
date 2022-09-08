8ba3d7bbad04844c124fc59173c7688efeddd346
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-08 22:39:29 +0900

    update: handle group private property with switch

--- components/GroupForm.tsx
+++ components/GroupForm.tsx
@@ -1,22 +1,26 @@
 // Formik x React Native example
 import React from "react";
-import { Button, TextInput, View } from "react-native";
+import { Button, TextInput, View, Switch, Text } from "react-native";
 import { Formik } from "formik";
 import { styles } from "../styles/styles";
 
 interface Group {
   groupName: string;
   groupDesciption: string;
-  private: string;
+  isPrivate: boolean;
 }
 
 export function GroupForm() {
-  const initialValues: Group = { groupName: "", groupDesciption: "" };
+  const initialValues: Group = {
+    groupName: "",
+    groupDesciption: "",
+    isPrivate: false,
+  };
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={(value: Group) => console.log(value)}>
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+      onSubmit={(values: Group) => console.log(values)}>
+      {({ handleChange, handleBlur, handleSubmit, setFieldValue, values }) => (
         <View>
           <TextInput
             style={styles("border:1", "p:1", "w:56", "m:5")}
@@ -32,13 +36,15 @@ export function GroupForm() {
             value={values.groupDesciption}
             placeholder={values.groupDesciption}
           />
-          <TextInput
-            style={styles("border:1", "p:1", "w:56", "m:5")}
-            onChangeText={handleChange("private")}
-            onBlur={handleBlur("private")}
-            value={values.private}
-            placeholder={values.private}
-          />
+          <View style={styles("flex:row")}>
+            <Text>Public</Text>
+            <Switch
+              value={values.isPrivate}
+              onValueChange={value => setFieldValue("isPrivate", value)}
+            />
+            <Text>Private</Text>
+          </View>
+
           <Button onPress={handleSubmit} title="Submit" />
         </View>
       )}
