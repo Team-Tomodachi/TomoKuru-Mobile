7aade53000b17f8ccc418d844ef0662d31d31858
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-10 13:50:26 +0900

    feat: add sign out button

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,14 +2,24 @@ import * as React from "react";
 import { Button, Text, View } from "react-native";
 import useAuthStore from "../store/auth";
 import { GroupForm } from "../components/GroupForm";
+import UserUtils from "../utils/user";
 
 export default function HomeScreen({ navigation }) {
-  const { isUserSignedIn } = useAuthStore();
+  const { isUserSignedIn, signUserOut } = useAuthStore();
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       {isUserSignedIn ? (
-        <GroupForm />
+        <View>
+          <Button
+            title="Sign Out"
+            onPress={() => {
+              UserUtils.handleSignOut();
+              signUserOut();
+            }}
+          />
+          <GroupForm />
+        </View>
       ) : (
         <Button
           onPress={() => navigation.navigate("Modal User")}
