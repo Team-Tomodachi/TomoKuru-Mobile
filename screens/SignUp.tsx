d7ee63700c49f6cc16b5912afa30f9a0ea486764
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-10 11:17:11 +0900

    feat: send user info to database
    Co-authored-by: adam0tokyo <adam0tokyo@users.noreply.github.com>

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -2,11 +2,35 @@ import { View, Text, TextInput, Alert, Pressable } from "react-native";
 import React, { useState } from "react";
 import { styles } from "../styles/styles";
 import UserUtils from "../utils/user";
+import useAuthStore from "../store/auth";
+import Constants from "expo-constants";
+const axios = require("axios").default;
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const { signUserIn } = useAuthStore();
+
+  //Handler
+
+  function addUserToDB(email: string, uid: string, name: string) {
+    axios
+      .post(`${Constants?.manifest?.extra?.apiURL}/api/users`, {
+        user_email: email,
+        firebase_id: uid,
+        first_name: name,
+        account_type: "user",
+      })
+      .then(function (response: JSON) {
+        console.log(response);
+      })
+      .catch(function (error: JSON) {
+        console.log(error);
+      });
+  }
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -20,8 +44,18 @@ export default function SignIn() {
         onChangeText={text => {
           setEmail(text);
         }}
-        autoCapitalize="none">
-      </TextInput>
+        autoCapitalize="none"></TextInput>
+      <Text style={styles("w:56", "text-align:justify")}>Name</Text>
+      <TextInput
+        style={styles("border:1", "p:1", "w:56", "m:5")}
+        placeholder="Name"
+        clearButtonMode="while-editing"
+        keyboardType="default"
+        returnKeyType="done"
+        onChangeText={text => {
+          setUsername(text);
+        }}
+        autoCapitalize="none"></TextInput>
       <Text style={styles("w:56", "text-align:justify")}>Password</Text>
       <TextInput
         style={styles("border:1", "p:1", "w:56", "m:5")}
@@ -32,10 +66,8 @@ export default function SignIn() {
         onChangeText={text => {
           setPassword(text);
         }}
-        autoCapitalize={'none'}
-        secureTextEntry={true}
-      >
-      </TextInput>
+        autoCapitalize={"none"}
+        secureTextEntry={true}></TextInput>
       <Text style={styles("w:56", "text-align:justify")}>Confirm password</Text>
       <TextInput
         style={styles("border:1", "p:1", "w:56", "m:5")}
@@ -46,12 +78,19 @@ export default function SignIn() {
         onChangeText={text => {
           setConfirmPassword(text);
         }}
-        autoCapitalize={'none'}
-        secureTextEntry={true}
-      >
-      </TextInput>
+        autoCapitalize={"none"}
+        secureTextEntry={true}></TextInput>
       <Pressable
-        onPress={() => UserUtils.handleSignUp(email, password, confirmPassword)}
+        onPress={async () => {
+          const user = await UserUtils.handleSignUp(
+            email,
+            password,
+            confirmPassword,
+          );
+          addUserToDB(String(user?.email), String(user?.uid), username);
+          // signUserIn();
+          // navigation.navigate("Home");
+        }}
         style={styles(
           "bg:green-600",
           "rounded:lg",
