4041b165e780883a70031d2446932d96bda92dc0
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-12 21:44:04 +0900

    fix: dismiss keyboard on touch and push input field up

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,4 +1,13 @@
-import { View, Text, TextInput, Alert, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import { styles } from "../styles/styles";
 import UserUtils from "../utils/user";
@@ -11,60 +20,68 @@ export default function SignIn({ navigation }) {
   const { isUserSignedIn, signUserIn } = useAuthStore();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={styles("w:56", "text-align:justify")}>Email</Text>
-      <TextInput
-        style={styles("border:1", "p:1", "w:56", "m:5")}
-        placeholder="Email"
-        clearButtonMode="while-editing"
-        keyboardType="email-address"
-        returnKeyType="done"
-        onChangeText={text => {
-          setEmail(text);
-        }}
-        autoCapitalize="none"></TextInput>
-      <Text style={styles("w:56", "text-align:justify")}>Password</Text>
-      <TextInput
-        style={styles("border:1", "p:1", "w:56", "m:5")}
-        placeholder="Password"
-        clearButtonMode="while-editing"
-        // keyboardType="email-address"
-        returnKeyType="done"
-        onChangeText={text => {
-          setPassword(text);
-        }}
-        autoCapitalize={'none'}
-        secureTextEntry={true}
-        ></TextInput>
-      <Pressable
-        onPress={() => {
-          UserUtils.handleSignIn(email, password);
-          signUserIn();
-          navigation.navigate("Home");
-        }}
-        style={styles(
-          "bg:green-600",
-          "rounded:lg",
-          "p:2",
-          "flex:row",
-          "justify:evenly",
-          "m:2",
-        )}>
-        <Text style={{ color: "white" }}>Sign In</Text>
-      </Pressable>
-      <Text>or</Text>
-      <Pressable
-        onPress={() => navigation.navigate("Modal User", { screen: "Sign Up" })}
-        style={styles(
-          "bg:orange-400",
-          "rounded:lg",
-          "p:2",
-          "flex:row",
-          "justify:evenly",
-          "m:2",
-        )}>
-        <Text>Sign Up</Text>
-      </Pressable>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text style={styles("w:56", "text-align:justify")}>Email</Text>
+          <TextInput
+            style={styles("border:1", "p:1", "w:56", "m:5")}
+            placeholder="Email"
+            clearButtonMode="while-editing"
+            keyboardType="email-address"
+            returnKeyType="done"
+            onChangeText={text => {
+              setEmail(text);
+            }}
+            autoCapitalize="none"></TextInput>
+          <Text style={styles("w:56", "text-align:justify")}>Password</Text>
+          <TextInput
+            style={styles("border:1", "p:1", "w:56", "m:5")}
+            placeholder="Password"
+            clearButtonMode="while-editing"
+            // keyboardType="email-address"
+            returnKeyType="done"
+            onChangeText={text => {
+              setPassword(text);
+            }}
+            autoCapitalize={"none"}
+            secureTextEntry={true}></TextInput>
+          <Pressable
+            onPress={() => {
+              UserUtils.handleSignIn(email, password);
+              signUserIn();
+              navigation.navigate("Home");
+            }}
+            style={styles(
+              "bg:green-600",
+              "rounded:lg",
+              "p:2",
+              "flex:row",
+              "justify:evenly",
+              "m:2",
+            )}>
+            <Text style={{ color: "white" }}>Sign In</Text>
+          </Pressable>
+          <Text>or</Text>
+          <Pressable
+            onPress={() =>
+              navigation.navigate("Modal User", { screen: "Sign Up" })
+            }
+            style={styles(
+              "bg:orange-400",
+              "rounded:lg",
+              "p:2",
+              "flex:row",
+              "justify:evenly",
+              "m:2",
+            )}>
+            <Text>Sign Up</Text>
+          </Pressable>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
