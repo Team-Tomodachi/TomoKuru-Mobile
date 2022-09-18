1c432cd137648b30ab283cd88e04ff86c2dc5d85
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-18 23:43:19 +0900

    feat: use mutation to patch user info with optimistic update

--- screens/UserCustomiseScreen.tsx
+++ screens/UserCustomiseScreen.tsx
@@ -1,20 +1,68 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import React, { useState } from "react";
 import { styles } from "../styles/styles";
 import { TextInput, Button } from "react-native-paper";
 import { Formik } from "formik";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import Constants from "expo-constants";
 
-export default function UserCustomiseScreen() {
+interface InfoToUpdate {
+  name: string;
+  cityWard: string;
+  prefecture: string;
+}
+
+export default function UserCustomiseScreen({ navigation }) {
   const [isUpdateDisabled, setUpdateDisabled] = useState<boolean>(true);
   const [isResetDisabled, setResetDisbaled] = useState<boolean>(true);
 
-  const initialValues = {
-    name: "Name",
-    email: "Email",
-    password: "",
-    cityWard: "",
-    prefecture: "",
-    contact: "",
+  const queryClient = useQueryClient();
+
+  const { first_name, city_ward, prefecture, email } = queryClient.getQueryData(
+    ["userInfo"],
+  );
+
+  const { mutate } = useMutation(
+    (values: InfoToUpdate) =>
+      axios.patch(
+        `${Constants?.expoConfig?.extra?.apiURL}/api/users/${email}`,
+        {
+          first_name: values.name,
+          city_ward: values.cityWard,
+          prefecture: values.prefecture,
+        },
+      ),
+    {
+      onMutate: async (values: InfoToUpdate) => {
+        await queryClient.cancelQueries(["userInfo"]);
+        const previousUserInfo = queryClient.getQueryData(["userInfo"]);
+        if (previousUserInfo) {
+          queryClient.setQueryData(["userInfo"], {
+            ...previousUserInfo,
+            first_name: values.name,
+            city_ward: values.cityWard,
+            prefecture: values.prefecture,
+          });
+          return { previousUserInfo };
+        }
+      },
+      onError: (err, variables, context) => {
+        console.log(err);
+        if (context?.previousUserInfo) {
+          queryClient.setQueryData(["userInfo"], context.previousUserInfo);
+        }
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(["userInfo"]);
+      },
+    },
+  );
+
+  const initialValues: InfoToUpdate = {
+    name: first_name,
+    cityWard: city_ward,
+    prefecture: prefecture,
   };
 
   const enableButtons = () => {
@@ -31,7 +79,11 @@ export default function UserCustomiseScreen() {
     <View style={styles("flex:1", "flex:col", "justify:center")}>
       <Formik
         initialValues={initialValues}
-        onSubmit={values => console.log(values)}>
+        onSubmit={values => {
+          mutate(values);
+          navigation.goBack();
+          Alert.alert("Succes", "Your info has been updated");
+        }}>
         {({ setFieldValue, handleSubmit, resetForm, values }) => (
           <>
             <TextInput
@@ -41,27 +93,6 @@ export default function UserCustomiseScreen() {
               clearButtonMode="while-editing"
               onChangeText={text => {
                 setFieldValue("name", text);
-                enableButtons();
-              }}
-            />
-            <TextInput
-              label="Email"
-              value={values.email}
-              placeholder={values.email}
-              clearButtonMode="while-editing"
-              onChangeText={text => {
-                setFieldValue("email", text);
-                enableButtons();
-              }}
-            />
-            <TextInput
-              label="Password"
-              value={values.password}
-              placeholder={values.password}
-              secureTextEntry={true}
-              clearButtonMode="while-editing"
-              onChangeText={text => {
-                setFieldValue("password", text);
                 enableButtons();
               }}
             />
@@ -85,20 +116,10 @@ export default function UserCustomiseScreen() {
                 enableButtons();
               }}
             />
-            <TextInput
-              label="Contact"
-              value={values.contact}
-              placeholder={values.contact}
-              clearButtonMode="while-editing"
-              onChangeText={text => {
-                setFieldValue("contact", text);
-                enableButtons();
-              }}
-            />
             <View style={styles("flex:col", "items:center", "my:5")}>
               <Button
                 disabled={isUpdateDisabled}
-                onPress={() => console.log("Pressed")}
+                onPress={handleSubmit}
                 style={styles(
                   "bg:green-500",
                   "rounded:lg",
