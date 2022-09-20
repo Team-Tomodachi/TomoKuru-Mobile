2ccfb8a07a63a39b769c5eba62ba45c25393d8cf
Adam <adam0tokyo@gmail.com>
2022-09-21 01:19:59 +0900

    feat: firebase image upload and download, hardocded values

--- screens/UserScreen.tsx
+++ screens/UserScreen.tsx
@@ -1,5 +1,5 @@
 import { Alert, Image, Pressable, View } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { styles } from "../styles/styles";
 import { Button } from "react-native-paper";
 import useAuthStore from "../store/auth";
@@ -7,50 +7,142 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import * as ImagePicker from "expo-image-picker";
 import { ActivityIndicator } from "react-native-paper";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { storage } from "../firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { getStorage, getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import uuid from 'react-native-uuid';
+import axios from "axios";
+import Constants from "expo-constants";
 
 
 export default function UserScreen({ navigation }) {
+  
+  const [image, setImage] = useState(null);
+  const [isUploading, setUploading] = useState<boolean>(false);
+  const [imageKey, setImageKey] = useState("");
+
+  //GET THIS WORKING
+  // need loading indicators for images
+  // const queryClient = useQueryClient();
+
+  // const { email } = queryClient.getQueryData(
+  //   ["userInfo"],
+  // );
+  // console.log(email)
+
+
+  async function loadUp() {
+    const pulledRef = await axios.get(`${Constants?.expoConfig?.extra?.apiURL}/api/users/adam1@test.com`) //don't hardcode this
+    // console.log("I pulled this", pulledRef.data.photo_url);
+    const fileRef = ref(getStorage(), pulledRef.data.photo_url); //user ID
+    const myImg = await getDownloadURL(fileRef)
+    //TODO !! solve getting user data
+  
+    setImage(myImg);
+  }
+
+    useEffect(() => {
+      loadUp();
+    }, []);
+  
+
   const { signUserOut } = useAuthStore();
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
-  const [image, setImage] = useState(null);
-  const [isUploading, setUploading] = useState<boolean>(false);
 
-  const pickImage = async () => {
-    if (!status?.granted) {
-      requestPermission();
-    }
-    if (status?.granted) {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: false,
-        aspect: [4, 3],
-        quality: 1,
-      });
-      if (!result.cancelled) {
-        setUploading(true);
-        const imageRef = ref(
-          storage,
-          `gs://tomokuru-auth.appspot.com/users/${uuid.v4()}-${Date.now()}`
-        );
-        uploadBytes(imageRef, result.uri).then(async (snapshot) => {
-          const url = await getDownloadURL(snapshot.ref);
-          setImage(url);
-        });
-        setTimeout(() => {
-          setUploading(false);
-          Alert.alert("Success", "Your profile picture has been updated");
-          // console.log(JSON.stringify(result));
-          // const now = Date.now()
-          // console.log(now, typeof now)
-        }, 5000);
+  async function uploadImageAsync(uri) {
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', uri, true);
+        xhr.send(null);
+    });
+    // const dbKey = `users/${userInfo.id}-${Date.now()}`;
+    const dbKey = `users/${uuid.v4()}-${Date.now()}`;
+    // console.log("dbKey", dbKey);
+    console.log("USER INFO", dbKey);
+    const fileRef = ref(getStorage(), dbKey); //user ID
+    const result = await uploadBytes(fileRef, blob);
+    console.log('the is RESULT!', result)
+    //upload to server
+    await axios.patch(
+      `${Constants?.expoConfig?.extra?.apiURL}/api/users/adam1@test.com`,
+      {
+        photo_url: dbKey,
       }
+    );
+
+
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    return await getDownloadURL(fileRef);
+}
+
+// getDownloadURL(starsRef)
+//   .then((url) => {
+//     // Insert url into an <img> tag to "download"
+//   })
+//   .catch((error) => {
+//     // A full list of error codes is available at
+//     // https://firebase.google.com/docs/storage/web/handle-errors
+//     switch (error.code) {
+//       case 'storage/object-not-found':
+//         // File doesn't exist
+//         break;
+//       case 'storage/unauthorized':
+//         // User doesn't have permission to access the object
+//         break;
+//       case 'storage/canceled':
+//         // User canceled the upload
+//         break;
+
+//       // ...
+
+//       case 'storage/unknown':
+//         // Unknown error occurred, inspect the server response
+//         break;
+//     }
+//   });
+
+const pickImage = async () => {
+  if (!status?.granted) {
+    requestPermission();
+  }
+  if (status?.granted) {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setUploading(true);
+      await uploadImageAsync(result.uri);
+      const pulledRef = await axios.get(`${Constants?.expoConfig?.extra?.apiURL}/api/users/adam1@test.com`) //don't hardcode this
+      // console.log("I pulled this", pulledRef.data.photo_url);
+      const fileRef = ref(getStorage(), pulledRef.data.photo_url); //user ID
+      const myImg = await getDownloadURL(fileRef)
+      //TODO !! solve getting user data
+
+      setImage(myImg);
+      // console.log("WHAT", upload);
+      // setImageURL(JSON.stringify(upload));
+      // console.log("ZE URL", imageURl);
+      setUploading(false);
+      Alert.alert("Success", "Your profile picture has been updated");
+
     }
-  };
-  //TODO clean up this function, improve filenmaming, test pulling it. push the change to db
+  }
+};
 
   return (
     <View style={styles("flex:1", "flex:col", "justify:start", "items:center")}>
