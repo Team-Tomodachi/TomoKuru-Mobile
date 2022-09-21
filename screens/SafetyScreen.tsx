8cd2f53d00e700b731c856b454fbc7b841a331c1
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-22 00:16:41 +0900

    feat: add check if user is signed in

--- screens/SafetyScreen.tsx
+++ screens/SafetyScreen.tsx
@@ -1,74 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { styles } from "../styles/styles";
 import { Button, Text, View, Alert } from "react-native";
-import Feather from "@expo/vector-icons/Ionicons";
+import Feather from "@expo/vector-icons/Feather";
 import * as SMS from "expo-sms";
 import * as Linking from "expo-linking";
 import useUserStore from "../store/user";
+import useAuthStore from "../store/auth";
 import axios from "axios";
 
 export default function SafetyScreen() {
-  // let [showToast, setShowToast] = useState(false);
   const [userData, setUserData] = useState("");
-
+  const { isUserSignedIn } = useAuthStore();
   const { email } = useUserStore();
-  console.log(email);
-
 
   useEffect(() => {
-    axios.get(`http://tomokuru.i-re.io/api/users/${email}`).then(function (response) {
-      setUserData(response.data);
-      console.log(response.data);
-      console.log("User Data", userData)
-      console.log("Contact", userData.contact)
-    });
+    axios
+      .get(`http://tomokuru.i-re.io/api/users/${email}`)
+      .then(response => setUserData(response.data));
   }, []);
 
   const noContactalert = () => {
     Alert.alert(
       "No Emergency Contact Found!",
-      "Please enter your emergency contact in settings",
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    )
-  }
+      "Please update your emergency contact in settings",
+    );
+  };
+
+  const notSignedIn = () => {
+    Alert.alert("Not signed in", "Please sign in");
+  };
 
   const sendDangerSMS = async () => {
-    if (userData.contact !== "" ){
+    if (!isUserSignedIn) {
+      notSignedIn();
+      return;
+    }
+    if (userData.contact === "") {
+      noContactalert();
+      return;
+    }
     await SMS.sendSMSAsync(
       `${userData.contact}`,
       "I arrived at the venue and do not feel comfortable. Please call me ASAP!",
     );
-    console.log("danger SMS has been sent");
-    }
-    else {
-      noContactalert();
-    }
   };
 
   const sendOKSMS = async () => {
-    if (userData.contact !== ""){
+    if (!isUserSignedIn) {
+      notSignedIn();
+      return;
+    }
+    if (userData.contact === "") {
+      noContactalert();
+      return;
+    }
     await SMS.sendSMSAsync(
       `${userData.contact}`,
       "I arrived at the venue everything is fine!",
     );
-    console.log("IM OKAY SMS has been sent");
-    }
-    else {
-      noContactalert();
-    }
   };
 
   const callCops = () => {
-    const url = "tel://08046381881";
+    const url = "tel://110";
     Linking.openURL(url);
   };
-
-// const callCops = () =>{
-//   const url="tel://110"
-//   Linking.openURL(url);
-// }
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -81,7 +76,7 @@ export default function SafetyScreen() {
           "justify:evenly",
           "m:10",
         )}>
-        <Feather name="star" color="white" size={30}></Feather>
+        <Feather name="user-check" color="white" size={30}></Feather>
         <Button title="I'M OK" color="green-600" onPress={sendOKSMS} />
       </View>
       <Text style={styles("text:2xl", "color:red-500", "m:5")}>EMERGENCY</Text>
@@ -94,7 +89,7 @@ export default function SafetyScreen() {
           "justify:evenly",
           "m:5",
         )}>
-        <Feather name="alert-sharp" color="white" size={30}></Feather>
+        <Feather name="alert-triangle" color="white" size={30}></Feather>
         <Button title="NOTIFY CONTACT" color="white" onPress={sendDangerSMS} />
       </View>
       <View
@@ -105,7 +100,7 @@ export default function SafetyScreen() {
           "flex:row",
           "justify:evenly",
         )}>
-        <Feather name="alert-circle-outline" color="white" size={30}></Feather>
+        <Feather name="alert-circle" color="white" size={30}></Feather>
         <Button title="NOTIFY POLICE" color="white" onPress={callCops} />
       </View>
     </View>
