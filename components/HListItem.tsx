8170d4831bc86828604ff223134ec7cf097b0e52
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-24 14:50:05 +0900

    fix: don't show picture if no photoUrl

--- components/HListItem.tsx
+++ components/HListItem.tsx
@@ -4,10 +4,9 @@ import { styles } from "../styles/styles";
 export default function HListItem({ imageUrl, name }) {
   return (
     <View style={{ margin: 10 }}>
-      {imageUrl ? (
-        <View
-          style={{ width: 150, height: 150, backgroundColor: "gray" }}></View>
-      ) : (
+      {/* {imageUrl ? ( */}
+      <View style={{ width: 150, height: 150, backgroundColor: "gray" }}></View>
+      {/* ) : (
         <Image
           source={{
             uri: imageUrl,
@@ -15,7 +14,7 @@ export default function HListItem({ imageUrl, name }) {
           style={{ width: 150, height: 150, backgroundColor: "gray" }}
           resizeMode="cover"
         />
-      )}
+      )} */}
       <View style={styles("absolute", "bg-opacity:0", "bottom:10", "left:1")}>
         <Text style={styles("color:white", "text:2xl")}>{name}</Text>
       </View>
