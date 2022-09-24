cb5e0b224cadab3274d8d96e4b6de879dbe8cfd8
stevemckillop <92012639+stevemckillop@users.noreply.github.com>
2022-09-24 11:03:38 +0900

    feat: photos rendering for Groups, Events

--- components/ListEvents.tsx
+++ components/ListEvents.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import EventListItem from "./EventListItem"
 
 const { height, width } = Dimensions.get("screen");
 
@@ -22,81 +23,16 @@ export default function ListEvents({ navigation }) {
     });
   }, []);
 
-  const shortenDescription = (description: any) => {
-    if (!description) {
-      return "description is empty";
-    } else if (description.length > 120) {
-      return description.slice(0, 120) + "...";
-    } else {
-      return description;
-    }
-  };
-
   return (
     <View>
       <ScrollView style={{ backgroundColor: "rgba(182, 182, 182, 1)" }}>
         {eventData.map((event, index) => {
           return (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate({
-                  name: "Event Details",
-                  params: { selectedEvent: eventData[index] },
-                });
-              }}
-              key={index}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  borderWidth: 0,
-                  borderRadius: 5,
-                  margin: 10,
-                  marginLeft: 15,
-                  marginRight: 15,
-                  paddingTop: 10,
-                  paddingBottom: 10,
-                  backgroundColor: "white",
-                }}
-                key={index}>
-                <Image
-                  style={{
-                    height: height * 0.1,
-                    width: width * 0.2,
-                    marginTop: 20,
-                    marginLeft: 20,
-                    marginRight: 50,
-                    marginBottom: 20,
-                  }}
-                  source={require("../DummyData/DummyEventPhotos/canada-world-cup.jpeg")}></Image>
-                <View
-                  style={{
-                    flexDirection: "column",
-                    width: width * 0.5,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 18,
-                      fontFamily: "OpenSans",
-                      fontWeight: "700",
-                    }}>
-                    {event.name}
-                  </Text>
-                  <Text
-                    style={{
-                      fontFamily: "OpenSans",
-                      fontStyle: "italic",
-                      color: "#8F8F8F",
-                    }}>
-                    {event.date}
-                  </Text>
-                  <Text style={{ fontFamily: "OpenSans" }}>
-                    {shortenDescription(event.description)}
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
+            <EventListItem singleEvent={event} key={index} /> 
+                )
+              }
+            )
+          }
       </ScrollView>
     </View>
   );
--- components/ListGroups.tsx
+++ components/ListGroups.tsx
@@ -7,13 +7,24 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
+import {
+  getStorage,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { styles } from "../styles/styles";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import GroupListItem from "./GroupListItem"
 
 const { height, width } = Dimensions.get("screen");
 
+
 export default function ListGroups({ navigation }) {
-  const [groupData, setGroupData] = useState([]);
+
+  const [groupData, setGroupData] = useState([])
 
   useEffect(() => {
     axios.get("http://tomokuru.i-re.io/api/groups").then(function (response) {
@@ -21,88 +32,19 @@ export default function ListGroups({ navigation }) {
     });
   }, []);
 
-  const shortenDescription = (description: any) => {
-    if (!description) {
-      return "description is empty";
-    } else if (description.length > 120) {
-      return description.slice(0, 120) + "...";
-    } else {
-      return description;
-    }
-  };
 
-  const isPrivate = (privacy: boolean) => {
-    return privacy === false ? "public" : "private";
-  };
+
   return (
     <View>
       <ScrollView style={{ backgroundColor: "#B6B6B6" }}>
         {groupData.map((group, index) => {
           return (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate({
-                  name: "Group Details",
-                  params: { selectedGroup: groupData[index] },
-                });
-              }}
-              key={index}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  borderWidth: 0,
-                  borderRadius: 5,
-                  margin: 10,
-                  marginLeft: 15,
-                  marginRight: 15,
-                  paddingTop: 10,
-                  paddingBottom: 10,
-                  backgroundColor: "white",
-                }}
-                key={index}>
-                <Image
-                  style={{
-                    height: height * 0.1,
-                    width: width * 0.2,
-                    marginTop: 20,
-                    marginLeft: 20,
-                    marginRight: 50,
-                    marginBottom: 20,
-                  }}
-                  source={require("../DummyData/DummyGroupPhotos/sunday-futsal-in-kinshicho.jpeg")}></Image>
-                <View
-                  style={{
-                    flexDirection: "column",
-                    width: width * 0.5,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 18,
-                      fontFamily: "OpenSans",
-                      fontWeight: "700",
-                    }}>
-                    {group.group_name}
-                  </Text>
-                  <Text
-                    style={{
-                      fontFamily: "OpenSans",
-                      fontStyle: "italic",
-                      color: "#8F8F8F",
-                    }}>
-                    {isPrivate(group.private) === "private"
-                      ? "Private Group"
-                      : "Public Group"}
-                    , {group.members_num} Members
-                  </Text>
-                  <Text style={{ fontFamily: "OpenSans" }}>
-                    {shortenDescription(group.group_description)}
-                  </Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
+            <GroupListItem singleGroup={group} key={index} /> 
+          )
+        })
+      }
       </ScrollView>
     </View>
-  );
+  )
 }
+    
