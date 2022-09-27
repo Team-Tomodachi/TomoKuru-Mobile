871f87fa97a0e497511d69fe6e66c504ff3fc4a8
hanhqvu <hanhqvu@users.noreply.github.com>
2022-09-27 23:37:16 +0900

    fix: sending correct collection ame

--- screens/EventDetailScreen.tsx
+++ screens/EventDetailScreen.tsx
@@ -81,7 +81,7 @@ export default function EventDetailScreen({ navigation, route }) {
           <Pressable
             onPress={() =>
               navigation.navigate('Messages', {
-                collectionName: `groups_${singleEvent.id}`,
+                collectionName: `events_${singleEvent.id}`,
               })
             }
           >
