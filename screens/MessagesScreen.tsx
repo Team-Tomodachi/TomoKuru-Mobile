8fdaa027ceda07d31bc1f4eabcf4bd35fa76a7d4
Hanh Vu <hanhqvu@users.noreply.github.com>
2022-09-29 10:53:28 +0900

    chores: remove unused codes

--- screens/MessagesScreen.tsx
+++ screens/MessagesScreen.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { firestore } from '../firebase';
 import { collection, addDoc, query, orderBy, limit, serverTimestamp, DocumentData } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { StyleSheet } from 'react-native';
 import useUser from '../hooks/useUser';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import { uuidv4 } from '@firebase/util';
@@ -11,7 +10,6 @@ export default function MessagesScreen({ route }) {
   const messagesRef = collection(firestore, `${route.params?.collectionName}`);
   const messageQuery = query(messagesRef, orderBy('timestamp'), limit(25));
   const [messages] = useCollectionData(messageQuery, { idField: 'id' });
-  const [messageToSend, setMessageToSend] = useState('');
   const [messagesToShow, setMessagesToShow] = useState([]);
 
   const getMessage = (firebaseMessage: DocumentData) => {
@@ -42,38 +40,6 @@ export default function MessagesScreen({ route }) {
   const { data } = useUser();
 
   return (
-    // <KeyboardAvoidingView
-    //   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    //   style={styles.container}>
-    //   <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    //     <>
-    //       <FlatList
-    //         data={messages}
-    //         keyExtractor={(item, index) => index}
-    //         renderItem={({ item }) => {
-    //           return <Text style={[Styling.actionButton]}>{item.message} -{item.user_name}</Text>;
-    //         }}
-    //       />
-    //       <TextInput placeholder="Message" style={styles.textInput} onChangeText={(text) => setMessageToSend(text)} value={messageToSend} />
-    //       <Pressable
-    //         disabled={messageToSend.length === 0}
-    //         style={Styling.actionButton}
-    //         onPress={() => {
-    //           addDoc(messagesRef, {
-    //             message: messageToSend,
-    //             timestamp: serverTimestamp(),
-    //             user_id: data.id,
-    //             user_name: data.first_name,
-    //             photo_url: data.photo_url,
-    //           });
-    //           setMessageToSend('');
-    //         }}
-    //       >
-    //         <Text style={Styling.actionButtonText}>SEND</Text>
-    //       </Pressable>
-    //     </>
-    //   </TouchableWithoutFeedback>
-    // </KeyboardAvoidingView>
     <GiftedChat
       messages={messagesToShow}
       onSend={messages =>
@@ -111,29 +77,3 @@ export default function MessagesScreen({ route }) {
     />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 5
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: 'space-around',
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48,
-  },
-  textInput: {
-    height: 40,
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-    marginBottom: 36,
-  },
-  btnContainer: {
-    backgroundColor: 'white',
-    marginTop: 12,
-  },
-});
